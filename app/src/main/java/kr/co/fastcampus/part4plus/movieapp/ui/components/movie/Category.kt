810a16779eebeb40adfc4e9a2c1391ceb754142a
PhilipY <philipy075@gmail.com>
2023-09-30 20:51:23 +0900

    p2_ch3_07_영화 카테고리 만들기

---
+++ app/src/main/java/kr/co/fastcampus/part4plus/movieapp/ui/components/movie/Category.kt
@@ -0,0 +1,67 @@
+package kr.co.fastcampus.part4plus.movieapp.ui.components.movie
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+
+@Composable
+fun CategoryRow(){
+    /**
+     * Type 이름 예) Action, Comedy...
+     * list --> Row...수평방향으로
+     */
+    Column(
+
+    ){
+        CategoryTitle(titleName = "Action")
+        LazyRow(
+            /**
+             * Item 하나가 가지는 패딩보다 더 큰 패딩...
+             */
+            contentPadding = PaddingValues(
+                horizontal = 10.dp
+            )
+        ){
+            /**
+             * for문이 아니라 items를 사용해야 한다. itemIndexed나...
+             */
+            item{
+                movieItem()
+            }
+            item{
+                movieItem()
+            }
+            item{
+                movieItem()
+            }
+            item{
+                movieItem()
+            }
+            item{
+                movieItem()
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryTitle(titleName: String) {
+    Text(
+        text = "Action",
+        modifier = Modifier.padding(10.dp)
+    )
+}
+
+@Preview
+@Composable
+fun CategoryRowPreview(){
+    CategoryRow()
+}
